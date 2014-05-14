879b1c7e369be2d318a4f51a80c2dba0d6666db0
Remus Rusanu <contact@rusanu.com>
2014-05-13 21:04:59 -0400

    separate loadMore from getItems, store items on transition for route re-visit (including browser back)

--- vendor/assets/javascripts/ember_rest_scroll.js
+++ vendor/assets/javascripts/ember_rest_scroll.js
@@ -5,59 +5,95 @@
     order_by: {
       id: 'asc'
     },
-    last_item: null,
+    lastItem: null,
+    loadedItems: null,
+    getItems: function() {
+      var loadedItems = this.get('loadedItems');
+      Ember.Logger.debug('getItems: ', loadedItems);
+      if (loadedItems)
+        return loadedItems;
+      this.set('lastItem', null);
+      return this.loadMore();
+    },
     loadMore: function() {
       var res = this.get('resource');
-      var lastItem = this.get('last_item');
+      var lastItem = this.get('lastItem');
       var params = {}
       if (lastItem) {
         params.key = lastItem.get('id');
       }
       var _this = this;
-      Ember.Logger.info('loadMore: resource: ', res, 
-        ' last_item:', this.get('last_item'),
+      Ember.Logger.debug('loadMore: resource: ', res, 
+        ' lastItem:', this.get('lastItem'),
         ' params:', params);
       return this.get('store').find(res, {rest_scroll: params}).then(
         function(items) {
             var content = items.get('content');
-            Ember.Logger.info('loadMore: then: ', items, ' content:', content);
+            Ember.Logger.debug('loadMore: then: ', items, ' content:', content);
             if (content.length > 0) {
               var lastItem = content[content.length - 1];
-              _this.set('last_item', lastItem);
-              Ember.Logger.info('last_item: set:', lastItem);
+              _this.set('lastItem', lastItem);
+              Ember.Logger.debug('lastItem: set:', lastItem);
             }
             return content;
         });
     },
     actions: {
       getMore: function() {
-        Ember.Logger.info("getMore:", this);
+        Ember.Logger.debug("getMore:", this);
         var _this = this;
         this.loadMore().then(
           function(items) {
             var c = _this.controllerFor(_this.routeName);
             c.gotMore(items);
           });
+      },
+      willTransition: function(transition) {
+        Ember.Logger.debug("willTransition: ", transition);
+        var controller = this.controllerFor(this.routeName);
+        var loadedItems = controller.get('content');
+        this.set('loadedItems', loadedItems);
+        return true;
       }
     }
   });
 
   RestScroll.ControllerMixin = Ember.Mixin.create({
     loadingMore: false,
+    hasMore: true,
     actions: {
       getMore: function() {
-        Ember.Logger.info("controller: getMore");
+        Ember.Logger.debug("controller: getMore");
         if (this.get('loadingMore')) return;
         this.set('loadingMore', true);
         this.get('target').send('getMore');
       },
     },
     gotMore: function(items) {
-        Ember.Logger.info("controller: gotMore");
+        Ember.Logger.debug("controller: gotMore");
         this.set('loadingMore', false);
-        this.pushObjects(items);
+        if (items.length) {
+          this.pushObjects(items);
+        }
+        else {
+          this.set('hasMore', false);
+        }
       }
     
+  });
+
+  RestScroll.LoadMoreView = Ember.View.extend({
+    didInsertElement: function() {
+      var _controller = this.get('controller');
+      Ember.Logger.debug("LoadMoreView: didInsertElement");
+      this.$().bind('inview', 
+        function(event, isInView, visibleX, visibleY) {
+          Ember.Logger.debug("inview: ", isInView, ' hasMore:', _controller.get('hasMore'));
+          if (isInView && _controller.get('hasMore')) {
+            _controller.send('getMore');
+          }
+        });
+    }
   });
 
   window.RestScroll = RestScroll;
